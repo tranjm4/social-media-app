a56950c6e3bfd38e752aec33cfccbd528aba4d17
Jonathan <jonathanmhtran@gmail.com>
2023-09-21 02:11:34 -0700

    Included CORS module

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+
 app.get("/login", (req, res) => {
-    res.send("Bruh");
+    res.json({ username: "Bruh", password: "Pluh" });
 })
 
 app.listen(PORT, () => {
